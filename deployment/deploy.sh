2df23fcf8cf3bbb78aad1ea82d690f7b9298eb8a
Emanuel Donalds <40231134+emanueldonalds@users.noreply.github.com.>
2024-10-16 21:22:46 +0300

    Update deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,8 +2,11 @@
 
 set -o nounset
 
-REPO_DIR=$HOME/property-viewer
-DEPL_DIR=$HOME/property-viewer-depl
+BASE_DIR=/home/properties
+APP_NAME=property-viewer
+
+REPO_DIR=$BASE_DIR/$APP_NAME
+DEPL_DIR="$REPO_DIR-depl"
 
 rm -rf $DEPL_DIR
 mkdir $DEPL_DIR
@@ -13,7 +16,7 @@ cp -r $REPO_DIR/src/assets $DEPL_DIR/assets
 cp $REPO_DIR/src/rss/template.xml $DEPL_DIR/rss/.
 
 cd $REPO_DIR/src
-env GOOS=linux GOARCH=amd64 /usr/local/go/bin/go build -o $DEPL_DIR/property-viewer
+env GOOS=linux GOARCH=amd64 /usr/local/go/bin/go build -o $DEPL_DIR/$APP_NAME
 
 sudo systemctl daemon-reload
-sudo systemctl restart property-viewer
+sudo systemctl restart $APP_NAME
--- deployment/install.sh
+++ deployment/install.sh
@@ -7,20 +7,25 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+APP_NAME=property-viewer
 REPO_DIR=$HOME/property-viewer
-OVERRIDE_CONF=/etc/systemd/system/property-viewer.service.d/override.conf
+DEPL_DIR="$HOME/$APP_NAME-dist"
+OVERRIDE_CONF=/etc/systemd/system/$APP_NAME.service.d/override.conf
 
-sudo ln -s $REPO_DIR/deployment/property-viewer.service /etc/systemd/system
+echo "Repo dir ${REPO_DIR}"
+echo "Deployment dir ${DEPL_DIR}"
+
+sudo ln -s $REPO_DIR/deployment/$APP_NAME.service /etc/systemd/system
         
 sudo touch $OVERRIDE_CONF
 
-if [ -d '/etc/systemd/system/property-viewer.service.d' ]; then
-        echo 'Env dir already created'
+if [ -d '/etc/systemd/system/$APP_NAME.service.d' ]; then
+    echo 'Env dir already created'
 else
-        sudo mkdir /etc/systemd/system/property-viewer.service.d
-        sudo echo "[Service]" >> $OVERRIDE_CONF
-        sudo echo "Environment=\"PROPERTY_VIEWER_DB_HOST=<db-host>\"" >> $OVERRIDE_CONF
-        sudo echo "Environment=\"PROPERTY_VIEWER_DB_PASSWORD=<db-password>\"" >> $OVERRIDE_CONF
-        echo "Created file '$OVERRIDE_CONF'. Set the environment variables in this file."
+    sudo mkdir /etc/systemd/system/$APP_NAME.service.d
+    sudo echo "[Service]" >> $OVERRIDE_CONF
+    sudo echo "Environment=\"PROPERTY_VIEWER_DB_HOST=<db-host>\"" >> $OVERRIDE_CONF
+    sudo echo "Environment=\"PROPERTY_VIEWER_DB_PASSWORD=<db-password>\"" >> $OVERRIDE_CONF
+    echo "Created file '$OVERRIDE_CONF'. Set the environment variables in this file."
 fi
 
